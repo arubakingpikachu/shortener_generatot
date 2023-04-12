8f78abd7c31ff76c04cfaa8abad904d39d8ef0e7
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-12 21:53:37 +0800

    feat:add generator route

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const shortenerGenerator=require('./shortener_ generator')
+const shortenGen=require('./shortener_ generator')
+const urlData=require('./models/url')
+const url = require('./models/url')
 require('./config/mongoose')
 
 const port = 3000
-
-
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -18,6 +18,31 @@ app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+
+
+//取得form的內容
+app.post('/',(req,res)=>{
+
+  if(!req.body.ori_url){return res.render('/')}
+
+  const ori_url=req.body.ori_url
+  const short_url=shortenGen(5)
+  console.log(short_url)
+  
+  urlData.findOne({originalUrl:ori_url})
+  .then(url=>{
+    if(!url){
+      urlData.create({shortenerUrl:`${short_url}`,originalUrl:`${ori_url}`})//假如找不到對應的URL，就創造一組新的URL跟短網址
+    }
+  })
+  .then(url=>{
+    console.log(short_url)
+    res.render('index',{origin:url.originalUrl,short:url.shortenerUrl,})
+  })
+  .catch(error => console.error(error))
+
 })
 
 app.listen(port, () => {
--- models/url.js
+++ models/url.js
@@ -6,7 +6,7 @@ const urlSchema=new Schema({
     type: String, 
     required: true 
   },
-  shortenerUri:{
+  shortenerUrl:{
     type: String, 
     required: true
   }
