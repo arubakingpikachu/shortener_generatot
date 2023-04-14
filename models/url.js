be407b5cb233439d6065888a3b9c14b593a49905
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-14 22:19:52 +0800

    feat:add copy text

--- models/url.js
+++ models/url.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const urlSchema=new Schema({
-  originalUrl:{
-    type: String, 
-    required: true 
+const urlSchema = new Schema({
+  originalUrl: {
+    type: String,
+    required: true
   },
-  shortenerUrl:{
-    type: String, 
+  shortenerUrl: {
+    type: String,
     required: true
   }
 })
--- shortener_ generator.js
+++ shortener_ generator.js
@@ -1,15 +1,13 @@
 
-  const element='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+const element = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
-module.exports=(shortener_length)=>{
-  let result=''
-  for(let i=0;i<shortener_length;i++){
-    const index=Math.floor(Math.random()*62) //取元素表中的index
-    const chooseElement=element[index]
-    result+=chooseElement
+module.exports = (shortener_length) => {
+  let result = ''
+  for (let i = 0; i < shortener_length; i++) {
+    const index = Math.floor(Math.random() * 62) // 取元素表中的index
+    const chooseElement = element[index]
+    result += chooseElement
   }
   console.log(result)
   return result
-  
 }
-
