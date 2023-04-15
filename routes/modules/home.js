12b1b4acb61b86fb1db6cb31abc2e5c8973e215e
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-04-15 23:40:18 +0800

    refactor:final check

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,7 +4,7 @@ const shortenGen = require('../../shortener_ generator')
 
 const router=express.Router()
 
-// 取得form的內容
+// 取得form的內容並render
 router.post('/', (req, res) => {
   if (!req.body.ori_url) { return res.render('/') }
 
@@ -15,7 +15,7 @@ router.post('/', (req, res) => {
     .then(url => {
       if (url) {
         res.render('index', { origin: url.originalUrl, short: url.shortenerUrl })
-        //若有找到對應的原url，就順便找出對應的短url並輸出
+        //若有找到對應的原url，就找出對應的短url並輸出，讓輸入相同網址時，會產生一樣的縮址
       } else { 
         urlData.create({ shortenerUrl: `${short_url}`, originalUrl: `${ori_url}` })
           .then(url => {
@@ -26,6 +26,7 @@ router.post('/', (req, res) => {
     })
     .catch(error => console.error(error))
 })
+
 // 用redirect讓短網址發揮作用
 router.get('/:path', (req, res) => {
   const { path } = req.params
